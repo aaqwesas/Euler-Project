9527bc5b1e1a41d164883dad579cb4028711635e
abc <123@gmail.com>
2025-07-27 17:25:19 +0800

    P16

---
+++ P16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+
+
+int main() {
+    boost::multiprecision::cpp_int n = 1;
+    n <<= 1000;
+
+    std::string s = n.str();
+    unsigned long long sum = 0;
+    for (char c : s) {
+        sum += c - '0'; // Convert char to digit
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}
