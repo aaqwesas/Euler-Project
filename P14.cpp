3a02e1f9db292dc068a06a11f2b48ee8ea6dbd3c
. <107795824+aaqwesas@users.noreply.github.com>
2025-07-28 15:17:32 +0800

    P11

--- P14.cpp
+++ P14.cpp
@@ -2,17 +2,23 @@
 #include <unordered_map>
 
 using namespace std;
-unsigned long long collatz(unsigned long long n, unordered_map<unsigned long long, unsigned long long>& map) {
-    if (map.count(n)) {
+unsigned long long collatz(unsigned long long n, unordered_map<unsigned long long, unsigned long long> &map)
+{
+    if (map.count(n))
+    {
         return map[n];
     }
-    if (n == 1) {
+    if (n == 1)
+    {
         return 1;
     }
     unsigned long long next;
-    if (n % 2 == 0) {
+    if (n % 2 == 0)
+    {
         next = n / 2;
-    } else {
+    }
+    else
+    {
         next = 3 * n + 1;
     }
     unsigned long long length = 1 + collatz(next, map);
@@ -20,16 +26,19 @@ unsigned long long collatz(unsigned long long n, unordered_map<unsigned long lon
     return length;
 }
 
-int main() {
+int main()
+{
     unordered_map<unsigned long long, unsigned long long> map;
     map[1] = 1;
     map[2] = 2;
     int limit = 1000000;
     unsigned long long max_length = 0;
     unsigned long long max_number = 0;
-    for (int i = 3; i <= limit; i++) {
+    for (int i = 3; i <= limit; i++)
+    {
         unsigned long long length = collatz(i, map);
-        if (length > max_length) {
+        if (length > max_length)
+        {
             max_length = length;
             max_number = i;
         }
--- P16.cpp
+++ P16.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <boost/multiprecision/cpp_int.hpp>
 
-
-int main() {
+int main()
+{
     boost::multiprecision::cpp_int n = 1;
     n <<= 1000;
 
     std::string s = n.str();
     unsigned long long sum = 0;
-    for (char c : s) {
+    for (char c : s)
+    {
         sum += c - '0'; // Convert char to digit
     }
     std::cout << sum << std::endl;
